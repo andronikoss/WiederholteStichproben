d05a6dceef60f4233e55b2c1b0fd55ee42af4b2b
andronikoss <and.stepanyan@gmail.com>
2014-07-22 12:44:22 +0200

    Update server.R

--- server.R
+++ server.R
@@ -8,8 +8,9 @@ shinyServer(function(input, output){
                 act <- input$action 
                 sigma <- input$sigma
                 Number.of.Observation <- input$Number.of.Observation
-                L <- reactive({data.gen(Number.of.Observation, alpha = 0.25, beta = 0.125, Number.of.Simulations=100,
-                                           sigma=sigma, exp.u=0, sim=act)})
+                L <- reactive({data.gen(Number.of.Observation, alpha = 0.2,
+                                        beta = 0.13, Number.of.Simulations = 100,
+                                           sigma = sigma, exp.u = 0, sim = act)})
                 
                 
                 type <- reactive({switch(input$select, 
@@ -18,18 +19,26 @@ shinyServer(function(input, output){
                 
                 select <- input$pick
                 
+#                 # Zeitangabe
+#                 output$time <- renderPrint({
+#                   t <- gsub("-", ".",Sys.time() )
+#                   t <- gsub(" ", ", ", t) 
+#                   cat("Stand:", t)
+#                 })
                           
                 dichte <- reactive({input$checkbox}) 
                 
-                calcul(L=L(), alpha = 0.25, beta=0.125, Number.of.Simulations=100,
-                       type=type(),dichte=dichte(), select=select)
+                calcul(L = L(), alpha = 0.2, beta = 0.13, Number.of.Simulations = 100,
+                       type = type(),dichte = dichte(), select = select)
                 
                 output$downloadPlot <- downloadHandler(
-                    filename = function() { paste("plot", '.jpeg', sep='') },
+                    filename = function() { paste("plot", '.jpeg', sep = '') },
                     content = function(file) {
                         jpeg(file,width = 1000, height = 600)
-                        print(calcul(L=L(), alpha = 0.25, beta=0.125, Number.of.Simulations=100,
-                                     type=type(),dichte=dichte(), select=select))
+                        print(calcul(L = L(), alpha = 0.2, beta = 0.13,
+                                     Number.of.Simulations = 100,
+                                     type = type(), dichte = dichte(),
+                                     select = select))
                         dev.off()
                     })
                           
