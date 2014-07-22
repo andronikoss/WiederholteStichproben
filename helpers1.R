95a763bb9f22542a28d3f99a429eb1cac1ddc146
andronikoss <and.stepanyan@gmail.com>
2014-07-22 12:41:46 +0200

    Update helpers1.R

--- helpers1.R
+++ helpers1.R
@@ -11,22 +11,23 @@
 ## sigma: Standardabweichung der Störgrößen
 ## sim: Kontrolle über den Zufallsprozess
 
-data.gen <- function(Number.of.Observation, alpha, beta, Number.of.Simulations=100,
-                     exp.u = 0, sigma = 0, sim=0){
+data.gen <- function(Number.of.Observation, alpha, beta,
+                     Number.of.Simulations = 100, exp.u = 0,
+                     sigma = 0, sim = 0){
     # Parameteranpassung
-    par(mfrow=c(1,3))
-    par(mar=c(5,5,2,5), mgp = c(3, 1.2, 0) )
+    par(mfrow = c(1, 3))
+    par(mar = c(5, 5, 2, 5), mgp = c(3, 1.2, 0) )
     
     # Der 'Seed' wird festgelegt in Abhängigkeit von 'sim' 
     set.seed(16 + sim)
     
     # Folgende Bedingung wird vorausgesetzt
         if(Number.of.Observation == 3){
-                x <- c(10,30,50)
+                x <- c(10, 30, 50)
         }else{
             repeat {
                 x <- rnorm(Number.of.Observation, 45, 18)
-                if ((length(which(x < 0)))==0){break}
+                if ((length(which(x < 0))) == 0){break}
             } 
             
         }
@@ -39,7 +40,7 @@ data.gen <- function(Number.of.Observation, alpha, beta, Number.of.Simulations=1
         N <- matrix(NA, Number.of.Simulations, Number.of.Observation)
         for (i in 1:Number.of.Simulations){
                 k <- rep(1,Number.of.Simulations)
-                y <- alpha + beta*x + rnorm(k[i]*Number.of.Observation,
+                y <- alpha + beta * x + rnorm(k[i] * Number.of.Observation,
                                             exp.u, sqrt(sigma))
                 ols <- lm(y ~ x)
                 M[i, 1:2] <- coef(ols)
