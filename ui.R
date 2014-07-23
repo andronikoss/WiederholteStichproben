c970c5384987e68a74b641d3a282ab60905b8b6a
andronikoss <and.stepanyan@gmail.com>
2014-07-23 17:19:59 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,8 +3,9 @@
 library(shiny)
 
 shinyUI( fluidPage(withMathJax(),
-    titlePanel(span("Wahrscheinlichkeitsverteilung der Kleinstquadrat-Schätzer",style="color:white"),
-               windowTitle="Dichte der KQ-Schätzer"),
+    titlePanel(span("Wahrscheinlichkeitsverteilung der Kleinstquadrat-Schätzer",
+                    style="color:white"),
+               windowTitle="KQ-Schätzer"),
     
 		sidebarLayout(position="right",
             sidebarPanel(
@@ -33,14 +34,14 @@ shinyUI( fluidPage(withMathJax(),
                     br(),
                            
                              
-                wellPanel(p(strong("Redaktion"), style='margin-bottom:1px;'),
-                            HTML("<p style='margin-bottom:1px;'>Programmierung: Andranik Stepanyan</p>"),
-                            p("Text: Ludwig von Auer"),
+                wellPanel(p(strong("Redaktion"), style='margin-bottom:1px;color:black;'),
+                            HTML("<p style='margin-bottom:1px;color:black;'>Programmierung: Andranik Stepanyan</p>"),
+                            p("Text: Ludwig von Auer", style="color:black"),
                             HTML("<a , style='margin-bottom:1px;color:black;' href = 'https://www.uni-trier.de/index.php?id=50126' target='_blank'>Professur für Finanzwissenschaft</a>"),
-                            p("Fachbereich IV - Volkswirtschaftslehre", style = 'margin-bottom:1px'),
-                            p("Universität Trier"),
-                            p(strong("Lehrbuch"), style = 'margin-bottom:1px'),
-                            HTML("<p>Auer, Ludwig <a href = 'https://www.uni-trier.de/index.php?id=15929' target='_blank'><img src='buch.jpg' style='float: right;'></a>von (2013),
+                            p("Fachbereich IV - Volkswirtschaftslehre", style = 'margin-bottom:1px;color:black;'),
+                            p("Universität Trier", style="color:black"),
+                            p(strong("Lehrbuch"), style = 'margin-bottom:1px; color:black;'),
+                            HTML("<p style = 'color:black;'>Auer, Ludwig <a href = 'https://www.uni-trier.de/index.php?id=15929' target='_blank'><img src='buch.jpg' style='float: right;'></a>von (2013),
                                  <a href = 'https://www.uni-trier.de/index.php?id=15929' target='_blank' style='color:black'>Ökonometrie - eine Einführung,<br>
                                  6. Auflage, Springer-Gabler<a/> </p>"),
                          
@@ -52,10 +53,12 @@ shinyUI( fluidPage(withMathJax(),
                                 <span class="caret"></span>
                                 </a>
                                 <ul class="dropdown-menu">
-                                <a href="https://oekonometrie.shinyapps.io/Intervallschaetzer/" target="_blank">Intervallschätzer</a>
-                                <p style="margin-bottom:1px;" ><a href="https://oekonometrie.shinyapps.io/ttest/" target="_blank">Trinkgeld-Beispiel: t-Test</a></p>
-                                <p style="margin-bottom:1px;"><a href="https://oekonometrie.shinyapps.io/Ftest/" target="_blank">Dünger-Beispiel: F-Test vs t-Test</a></p>
-								<p style="margin-bottom:1px;"><a href="https://oekonometrie.shinyapps.io/resid/" target="_blank">Wahrscheinlichkeitsverteilung der Störgrößen</a></p>
+                                <p style="margin-bottom:1px;"><a href="https://oekonometrie.shinyapps.io/Stoergroessen/" target="_blank">&nbsp; Störgrößen</a></p>
+                                <p style="margin-bottom:1px;color:black;">&nbsp; KQ-Schätzer</p>
+                                <a href="https://oekonometrie.shinyapps.io/Intervallschaetzer/" target="_blank">&nbsp; Intervallschätzer</a>
+                                <p style="margin-bottom:1px;" ><a href="https://oekonometrie.shinyapps.io/t-Test/" target="_blank">&nbsp; t-Test</a></p>
+                                <p style="margin-bottom:1px;"><a href="https://oekonometrie.shinyapps.io/Ftest/" target="_blank">&nbsp; F-Test versus t-Test</a></p>
+								
                                 </ul>
                                 </div>')),
                 list(tags$head(tags$style("body {background-color: #6d6d6d; }"))))
@@ -74,7 +77,7 @@ tabPanel(h5("Was wird veranschaulicht?"),
 p("Jede wiederholte Stichprobe erzeugt eine neue Punktwolke und damit neue 
 KQ-Schätzwerte \\( \\hat{\\alpha} \\) und \\( \\hat{\\beta} \\).  In der 
 Animation können Sie den Einfluss der Störgrößenvarianz und des 
-Beobachtungsumfangs auf diese KQ-Schätzwerte studieren." ) ),
+Beobachtungsumfangs auf diese KQ-Schätzwerte studieren.", style="color:black" ) ),
 
 tabPanel(h5("Was zeigt die Anfangseinstellung?"), p("Die Animation greift das 
 Trinkgeld-Beispiel des Lehrbuches auf. Für jeden Gast \\( t \\) wird das 
@@ -86,19 +89,20 @@ Der Animation sind die wahren Parameterwerte \\( \\alpha = 0,2 \\)   und  \\(
 und \\( x_{3}=50 \\) zugrunde gelegt . Ferner wurden normalverteilte 
 Störgrößen \\( u_{t} \\) mit Erwartungswert \\( E(u_{t})=0 \\) unterstellt. In
 der Anfangseinstellung beträgt die Störgrößenvarianz \\( {\\sigma}^2 = 2 \\) 
-und der Beobachtungsumfang (Anzahl der Beobachtungen pro Stichprobe,) beträgt 
+und der Beobachtungsumfang (Anzahl der Beobachtungen pro Stichprobe) beträgt 
 \\( T=3 \\).",
 
-HTML("<ul><li>Die <strong>linke Grafik</strong> zeigt die Punktwolke, welche 
+HTML("<ul><li style='color:black;'>Die <strong>linke Grafik</strong> zeigt die Punktwolke, welche 
 auf der Grundlage dieser Parameterwerte in einem Zufallsprozess erzeugt wurde.
 Die rote Linie ist die resultierende KQ-Regressionsgerade.</li></ul>"), 
-HTML("<ul><li>Die rote Gerade der <strong>mittleren Grafik</strong> markiert 
+HTML("<ul><li style='color:black;'>Die rote Gerade der <strong>mittleren Grafik</strong> markiert 
 den \\( \\hat{\\beta} \\)-Wert (Steigung) dieser Regressionsgerade, die 
 schwarze Gerade den wahren Wert \\( \\beta = 0,13 \\) . Die graue Fläche kann 
-zunächst noch ignoriert werden.</li></ul>"), HTML("<ul><li>Die rote Gerade der
+zunächst noch ignoriert werden.</li></ul>"),
+HTML("<ul><li style='color:black;'>Die rote Gerade der
 <strong>rechten Grafik</strong> markiert den \\( \\hat{\\alpha} \\)-Wert der 
 Regressionsgerade (Schnittpunkt mit der vertikalen Achse) und die schwarze 
-Gerade den wahren Wert \\( \\alpha = 0,2 \\).</li></ul>")
+Gerade den wahren Wert \\( \\alpha = 0,2 \\).</li></ul>"), style="color:black" 
 
 )),
 
@@ -108,7 +112,7 @@ HTML("<img src='slider.jpg'>"), ", mit denen Sie die zugrunde
 liegenden Parameterwerte verändern können. Klicken Sie dafür mit der linken 
 Maustaste auf den entsprechenden Schieber und bewegen sie ihn nach rechts oder
 links.",
-HTML("<p>Mit dem Stichproben-Schieber stellen Sie ein, wie viel 
+HTML("<p style ='color:black;'>Mit dem Stichproben-Schieber stellen Sie ein, wie viel 
 wiederholte Stichproben \\( S \\) erzeugt werden sollen. Jeder wiederholten 
 Stichprobe liegen die gleichen Rechnungsbeträge und die gleichen 
 Regressionsparameter ( \\( \\alpha = 0,2 \\) und \\( \\beta =0,13 \\)) 
@@ -116,15 +120,15 @@ zugrunde. Die zufallsabhängigen Störgrößen \\( u_{t} \\) sorgen dafür, dass
 die Trinkgeldbeträge \\( y_{t} \\) in jeder wiederholten Stichprobe dennoch 
 unterschiedlich ausfallen. Es werden folglich \\( S \\) verschiedene 
 Punktwolken und damit \\( S \\) verschiedene KQ-Regressionsgeraden 
-erzeugt.</p>"), "Wenn Sie im Bedienfenster das Feld" , 
-HTML("<img src='stich.jpg'>") ,"anklicken, werden \\( S \\) neue Stichproben erzeugt und 
-die alten werden gelöscht.",
-HTML('<p>Wie die \\( S \\) Stichproben in der 
+erzeugt.</p>"), HTML("<p style ='color:black;'>Wenn Sie im Bedienfenster das Feld
+<img src='stich.jpg'> anklicken, werden \\( S \\) neue Stichproben erzeugt und 
+die alten werden gelöscht.</p>"),
+HTML('<p style ="color:black;">Wie die \\( S \\) Stichproben in der 
 linken Grafik dargestellt werden sollen, legen Sie selbst fest. Im Bedienfeld 
 haben Sie unter der Überschrift "Darstellung" die Wahl zwischen <img 
 src="Einzeln.jpg"> und <img src="Gemeinsam.jpg"> Die Voreinstellung ist <img 
 src="Einzeln.jpg">'),
-HTML("<dl class='dl-horizontal'> <dt><img src='Einzeln.jpg'></dt> <dd>In der 
+HTML("<dl class='dl-horizontal'> <dt><img src='Einzeln.jpg'></dt> <dd style ='color:black;'>In der 
 linken Grafik wird die erste der \\( S \\) 
 Stichproben einzeln dargestellt. Die Grafik zeigt die Punktwolke und die 
 Regressionsgerade dieser Stichprobe. Klicken Sie nun auf",
@@ -141,7 +145,7 @@ langsam abläuft, können Sie den Stichproben-Schieber einfach auf \\( 100 \\)
 erhöhen. Die linke Grafik zeigt dann die Punktwolke und die Regressionsgerade
 der \\( 100 \\). Stichprobe an und die Histogramme in der mittleren und
 rechten Grafik berücksichtigen alle \\( 100 \\) KQ-Schätzwerte.</dd></dl>"),
-HTML("<dl class='dl-horizontal'> <dt><img src='Gemeinsam.jpg'></dt> <dd>Bei
+HTML("<dl class='dl-horizontal'> <dt><img src='Gemeinsam.jpg'></dt> <dd style ='color:black;'>Bei
 dieser Darstellungsform werden die \\( S \\) Regressionsgeraden gemeinsam in
 der linken Grafik angezeigt, wobei die Regressionsgerade der letzten
 Stichprobe schwarz markiert ist. Die Punktwolken werden allerdings
@@ -149,18 +153,32 @@ weggelassen. Die Häufigkeitsverteilungen der KQ-Schätzwerte \\( \\hat{\\alpha}
 \\) und \\( \\hat{\\beta} \\) sind auch bei dieser Darstellungsform in der
 mittleren und rechten Grafik zu sehen. Wenn Sie im Bedienfenster das <img
 src='Start.jpg'> Feld anklicken, werden schrittweise weitere Stichproben 
-hinzugefügt.</dd></dl>"), "Um den Einfluss des Beobachtungsumfangs auf die 
+hinzugefügt.</dd></dl>"),
+HTML('<p style ="color:black;">Um den Einfluss des Beobachtungsumfangs auf die 
 Histogramme zu erkennen, sollten Sie im Bedienfenster den 
 Beobachtungsumfang-Schieber nach rechts bewegen. Die KQ-Schätzwerte 
 konzentrieren sich daraufhin enger um die wahren Werte. Der gleiche Effekt 
-ergibt sich, wenn der Störgrößenvarianz-Schieber nach links bewegt wird." , 
-'Wenn Sie bei', strong("Dichte") ,'einen Haken setzen, wird zusätzlich zum 
+ergibt sich, wenn der Störgrößenvarianz-Schieber nach links bewegt wird. 
+Wenn Sie bei <strong>Dichte</strong> einen Haken setzen, wird zusätzlich zum 
 Histogramm eine Schätzung der Wahrscheinlichkeits(dichte)funktion vorgenommen,
-also ein „geglättetes Histogramm" erzeugt.' ,
-HTML('<p>Um die aktuelle Grafik in einer jpg-Datei zu speichern, klicken Sie auf <img 
+also ein „geglättetes Histogramm" erzeugt.</p>'),
+
+HTML('<p style ="color:black;">Um die aktuelle Grafik in einer jpg-Datei zu speichern, klicken Sie auf <img 
 src="download.jpg">.</p>'),
-HTML("<p>Um Animationen zu anderen ökonometrischen Themen zu sehen,
-     klicken Sie bitte auf <img src = 'info.jpg'>.</p>")))), 
+
+HTML("<p style ='color:black;'>Um Animationen zu anderen ökonometrischen Themen zu sehen,
+     klicken Sie bitte auf <img src = 'info.jpg'>.</p>"), style="color:black")),
+tabPanel(h5("Details"), p("Die Animation ist mit dem statistischen
+Programm R erzeugt worden. Die Dichte
+der Parameter \\( \\alpha \\) und \\( \\beta \\) wurde mit Gaußkern
+ermittelt. Für die Wahl der Bandbreite wurde die Faustregel von Silverman 
+herangezogen (Silverman, B. W., 1986, Density Estimation for Statistics
+and Data Analysis, Chapman and Hall, London, S. 47 f).",
+HTML("<p style='color:black;'>Die R-Skripte für die Reproduktion
+dieser Seite sind unter folgendem Link aufrufbar:
+<a href='https://github.com/andronikoss/WiederholteStichproben' 
+     target='_blank'>R Codes.</a></p>"), style="color:black"
+))),
 br(),
 br(), 
 br())
