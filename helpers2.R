2c6a73ce373c63fbc1b08eb1ca89c208ec6ff7bd
andronikoss <and.stepanyan@gmail.com>
2015-03-26 12:48:36 +0100

    Update helpers2.R

--- helpers2.R
+++ helpers2.R
@@ -1,4 +1,5 @@
 
+
 # Der Befehl calcul ermöglicht die Visualisierung
 # ----
 # Die Parameter sind:
@@ -10,7 +11,7 @@
 ## type: Die grafische Darstellun der Regressionsgeraden 
 ## dichte: Die geschätzte Dichtefunktion wird aktiviert
 ## select: Die Anzahl von Stichproben, die man abbilden will
-
+library(MASS)
 calcul <- function(L, alpha = 0.2, beta = 0.13, Number.of.Simulations = 100,
                    type = c("einzeln", "alle"),
                    dichte = FALSE, select = select){
@@ -21,7 +22,7 @@ calcul <- function(L, alpha = 0.2, beta = 0.13, Number.of.Simulations = 100,
     
     # Parameteranpassungen
     par(mfrow = c(1, 3))
-    par(mar = c(5, 5, 5, 1), mgp = c(3, 1.2, 0), oma = c(0, 0, 0, 0))
+    par(mar = c(7, 5, 5, 1), mgp = c(3, 1.2, 0), oma = c(0, 0, 0, 0))
     select <- as.integer(select)
     
     
@@ -29,15 +30,18 @@ calcul <- function(L, alpha = 0.2, beta = 0.13, Number.of.Simulations = 100,
     if(type == "alle"){
         plot(seq(1, 100,length = 10), seq(-5, 20, length = 10),
              type = "n", xlab = "", ylab = "", main = "",
-             cex.axis = 1.5)
+             axes=F, xaxs="i", yaxs="i")
+        axis(1, at=0, labels=0,cex.axis = 1.8)
+        axis(1, at=seq(0,100,20),cex.axis = 1.8)
+        axis(2,cex.axis = 1.8, las=1)
         if(select == 1){
                 title(main = paste("KQ-Regressionsgerade \n der 1. Stichprobe"),
                       xlab = "Rechnungsbetrag", ylab="Trinkgeld", cex.main = 1.8,
-                      cex.lab = 1.5, font.main = 1)
+                      cex.lab = 1.8, font.main = 1)
         }else{
                 title(main = paste("KQ-Regressionsgeraden \n aller", select, "Stichproben"),
                       xlab = "Rechnungsbetrag", ylab = "Trinkgeld", cex.main = 1.8,
-                      cex.lab = 1.5, font.main = 1)
+                      cex.lab = 1.8, font.main = 1)
         }
      
         pal <- gray.colors(Number.of.Simulations, start = 0.1,
@@ -53,11 +57,14 @@ calcul <- function(L, alpha = 0.2, beta = 0.13, Number.of.Simulations = 100,
     if(type == "einzeln"){
         plot(seq(1, 100, length = 10), seq(-5, 20, length = 10), type = "n",
              xlab = "", ylab = "", main = "",
-             cex.axis = 1.5)
+              axes=F, xaxs="i", yaxs="i")
+
+        axis(1, at=seq(-20,100,20), cex.axis = 1.8)
+        axis(2, cex.axis = 1.8, las=1)
         l <- paste(select, ".")
         l <- gsub(" ", "", l)
         title(main = paste("KQ-Regressionsgerade \n der", l, "Stichprobe"), 
-              xlab = "Rechnungsbetrag", ylab="Trinkgeld", cex.main =1.8, cex.lab = 1.5,
+              xlab = "Rechnungsbetrag", ylab="Trinkgeld", cex.main = 1.8, cex.lab = 1.8,
               font.main = 1)
         abline(M[select, ], col = "red", lwd = 1.2)
         abline(h = 0, lty = 2)
@@ -71,7 +78,7 @@ calcul <- function(L, alpha = 0.2, beta = 0.13, Number.of.Simulations = 100,
  # o Erste Grafik für Steigungsparameter
  # -------   
     # Parameteranpassungen
-    par(mar = c(15,5.5,15,1), mgp = c(4, 2, 0))
+    par(mar = c(7,6,5,1), mgp = c(4, 2.4, 0))
     
     # Das Histogram für den Steigungsparameter 'beta' wird gebildet
     hist.den.beta <- hist(M[1:select, 2], plot = F,breaks = 15)
@@ -85,16 +92,23 @@ calcul <- function(L, alpha = 0.2, beta = 0.13, Number.of.Simulations = 100,
     }
  
     # Das Histogramm wird geplottet
-    hist(M[1:select, 2],breaks = 15,freq = F,col = "lightgrey",
-         main = "",
-         ylim = c(0, max(den.beta$y, hist.den.beta$density)), xlab = "", ylab = "",
-         cex.axis = 1.5)
-    
+ truehist(M[1:select, 2],nbins = 15,col = "lightgrey",
+         main = "",xlim=c(0, 0.25),
+         ylim = c(0, 60), xlab = "", ylab = "", xaxs="i", yaxs="i",
+         axes=F)
+#  mtext(side=2, "Dichte", cex=1.2)
+ axis(1,cex.axis = 1.8)
+ axis(2, labels=F, tck=-0.03)
     # Die Achsen werden entsprechend definiert
-    title( xlab = bquote(hat(beta)),
-          ylab = "Dichte", cex.main = 1.8, cex.lab = 1.4, font.main = 1)
+    title(main=expression(paste("Histogramm ", (beta))),cex.main = 1.8)
+    
+    par(mar = c(7,6,5,1), mgp = c(2.5, 2.4, 0))
+    title(ylab = "Dichte", cex.lab = 1.8, font.lab = 1)
+
+    par(mar = c(7,6,5,1), mgp = c(4.7, 2, 0))
+    title(xlab = bquote(hat(beta)), cex.main = 1.8, cex.lab = 1.8, font.main = 1)
     # Überschrift wird ergänzt
-    mtext(expression(paste("Histogramm ", (beta))), side = 3, cex = 1.2)
+#     mtext(expression(paste("Histogramm ", (beta))), side = 3, cex = 1.2)
     
     # Für den Fall mit nur einer Stichprobe wird folgende Bedingung definiert.
     if(dichte == TRUE & select != 1){
@@ -107,17 +121,17 @@ calcul <- function(L, alpha = 0.2, beta = 0.13, Number.of.Simulations = 100,
  
     #abline(v=mean(M[,2]), lty=2)
     # Parametteranpassung
-    par(mgp = c(4, 1, 0))
+    par(mgp = c(4, 1.3, 0))
     
     # Beta-Wert wird auf der x-Achse angezeigt 
-    axis(1, at = beta, labels = expression(beta), cex.axis = 1.3)
-    abline(v = beta, lwd = 2)
+    axis(1, at = beta, labels = expression(beta), cex.axis = 1.8)
+    abline(v = beta, lwd = 1)
  
  # -------
  # o Zweite Grafik für Niveauparameter
  # -------
     # Parameteranpassung
-    par(mar = c(15,5.5,15,1), mgp = c(4, 2, 0))
+    par(mar = c(7,6,5,1), mgp = c(4, 2.4, 0))
     hist.den.alpha <- hist(M[1:select, 1], plot = F, breaks = 15)
     
     if(select != 1){
@@ -126,15 +140,21 @@ calcul <- function(L, alpha = 0.2, beta = 0.13, Number.of.Simulations = 100,
         den.alpha <- list(y = 0)
     }
     # Histogramm wird abgebildet
-    hist(M[1:select, 1], freq = F, col = "lightgrey", breaks = 15,
-         main = "", ylim = c(0, max(den.alpha$y, hist.den.alpha$density)),
-         xlab = "", ylab = "", cex.axis = 1.5, font.main = 1)
+    truehist(M[1:select, 1], col = "lightgrey", nbins = 15,
+         main = "", ylim = c(0, 2),xlim=c(-4,4), xaxs="i", yaxs="i",
+         xlab = "", ylab = "",  font.main = 1, axes=F)
+ axis(1,cex.axis = 1.8)
+ axis(2, labels=F, tck=-0.03)
+#  mtext(side=2, "Dichte", cex=1.2)
     # Die Achsen werden entsprechend beschriftet
-    title(ylab = "Dichte",xlab = bquote(hat(alpha)),
-          cex.main = 1.8, cex.lab = 1.4)
- 
+    title(main=expression(paste("Histogramm ", (alpha))),cex.main = 1.8)
+    par(mar = c(7,6,5,1), mgp = c(2.5, 2.4, 0))
+    title(ylab = "Dichte", cex.lab = 1.8, font.lab = 1)
+    
+    par(mar = c(7,6,5,1), mgp = c(4.7, 2.4, 0))
+    title(xlab = bquote(hat(alpha)), cex.lab = 1.8, font.lab = 1)
     # Überschrift wird ergänzt
-    mtext(expression(paste("Histogramm ", (alpha))),side = 3, cex = 1.2)
+#     mtext(expression(paste("Histogramm ", (alpha))),side = 3, cex = 1.2)
  
     # Für den Fall mit nur einer Stichprobe wird folgende Bedingung definiert.
     if(dichte == TRUE & select != 1){
@@ -145,9 +165,9 @@ calcul <- function(L, alpha = 0.2, beta = 0.13, Number.of.Simulations = 100,
             abline(v = M[select, 1], col = "red", lwd = 1.5)
     }
     #abline(v=mean(M[,1]), lty=2)
-    par(mgp = c(4, 1, 0))
-    axis(1, at = alpha, labels = expression(alpha), cex.axis = 1.3)
-    abline(v = alpha, lwd = 2)
+    par(mgp = c(4, 0.8, 0))
+    axis(1, at = alpha, labels = expression(alpha), cex.axis = 1.8)
+    abline(v = alpha, lwd = 1)
     
     # Parameteranpassung
     par(mfrow = c(1,1))
